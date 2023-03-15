08f49eb72ac73dc1de540baeebc54488e12de85c
Alex Náger <51228484+aleexnager@users.noreply.github.com>
2023-03-15 19:16:23 +0100

    Add files via upload

--- autoUpdate.sh
+++ autoUpdate.sh
@@ -4,29 +4,43 @@
 conf_file="config.cfg"
 if [ -r "$conf_file" ]; then
     source "$conf_file"
-    echo -e "\"$conf_file\" read correctly"
 else
     echo -e "\e[1mWARNING:\e[0m \"$conf_file\" could not open. Program mode set to default."
     auto=0
+    prevDate=2002-02-20
 fi
 
-#Update-upgrade
+#---------------FUNCTIONS---------------
+#config.cfg
+function conf0(){ #write in conf_file for auto=0
+    echo password="$password" > "$conf_file" #write in conf_file
+    echo prevDate="$prevDate" >> "$conf_file"
+    echo auto="$auto" >> "$conf_file"
+}
+
+function conf1(){ #write in conf_file for auto=1
+    echo password="$password" > "$conf_file" #write in conf_file
+    echo prevDate="$today" >> "$conf_file"
+    echo auto="$auto" >> "$conf_file"
+}
+
+#update-upgrade
 function update(){
-    echo 20022539 | sudo -S apt-get update
+    echo $password | sudo -S apt-get update
     sudo apt-get upgrade -y
 
     if [ $? -eq 0 ]; then
 	sudo apt-get autoclean
 	sudo apt-get clean
 	sudo apt-get autoremove
-	echo -e "Updates installed successfully.\n"
+	echo -e "\nUpdates installed successfully.\n"
     else
 	echo -e "\e[1mWARNING:\e[0m Updates failed to install.\n"
 	exit 255
     fi
 }
 
-#HELP AND INFO
+#help && info
 function help(){
     #el flag -e interpreta los '\'
     #\e indicates ini-end of the style
@@ -45,25 +59,25 @@ The program has 2 modes.\n\
 2. Automatic mode: in this mode the program will work by its own, it will execute once a day.\n"
 }
 
-#ARGUMENT TREATMENT
+#---------------ARGUMENT TREATMENT---------------
 for arg in "$@"; do
-    if [ "$arg" == "-h" ] || [ "@arg" == "--help" ]; then
+    if [ "$arg" == "-h" ] || [ "$arg" == "--help" ]; then #help && info
 	help
 	info
-        exit 255
-    elif [ "$arg" == "-auto" ]; then #Automated version
+        exit 0
+    elif [ "$arg" == "-auto" ]; then #modes
 	info
 	if [ $auto -eq 0 ]; then
 	    echo "You are actually in manual mode. Do you want to change to automatic mode? [yes/no]"
             read answer #stdin
 	    if [ "$answer" = "no" ]; then
                 echo -e "You will stay with the manual version. Use \e[1m./autoUpdate.sh\e[0m to update your machine\n"
-                exit 255
+                exit 0
             elif [ "$answer" = "yes" ]; then
                 echo -e "You have changed to the automatic version. Your machine will update daily.\n"
                 auto=1
-	        echo auto="$auto" > "$conf_file" #write in config_file
-	        exit 255
+	        conf0
+	        exit 0
             else
                 echo -e "Unknown command option, use --help/-h to ask for help and info about the program\n"
                 exit 255
@@ -73,18 +87,16 @@ for arg in "$@"; do
 	    read answer
 	    if [ "$answer" = "no" ]; then
                 echo -e "You will stay with the automatic version. Your machine will update daily\n"
-                exit 255
+                exit 0
             elif [ "$answer" = "yes" ]; then
                 echo -e "You have changed to the manual version. Use \e[1m./autoUpdate.sh\e[0m to update your machine.\n"
-               auto=0
-	        echo auto="$auto" > "$conf_file"
-	        exit 255
+                auto=0
+	        conf0
+	        exit 0
             else
                 echo -e "Unknown command option, use --help/-h to ask for help and info about the program\n"
                 exit 255
 	    fi
-	else
-	    echo -e "\e[1mWARNING:\e[0m the value of the variable \"auto\" is not recognizable"
 	fi
     else
         echo -e "Unknown command option, use --help/-h to ask for help and info about the program\n"
@@ -92,22 +104,23 @@ for arg in "$@"; do
     fi
 done
 
-
-if [ $auto -eq 0 ]; then
-    update
-    exit 0
-fi
-
+#---------------MAIN---------------
 if [ $auto -eq 1 ]; then
     today=$(date +"%Y-%m-%d")
     if [ "$today" = "$prevDate" ]; then
 	echo "You are up to date. :)"
-	#control = 1 > config_file
+	conf1
 	exit 0
     else
+	echo -e "Your machine has not been updated since: $prevDate \n"
+	conf1
 	update
-	#prevDate = today > config_file
 	exit 0
     fi
+else 
+    conf0
+    update
+    exit 0
 fi
+
 exit 0
